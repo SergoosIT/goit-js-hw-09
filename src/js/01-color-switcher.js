28267298cb118a7da779305de7c7e3849688ef99
Сергей Смирнов <sergio.feriny@gmail.com>
2021-11-12 00:15:20 +0200

    hw9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const ref = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+
+let timer = null;
+
+ref.btnStart.addEventListener('click', onBtnStartClick);
+ref.btnStop.addEventListener('click', onBtnStopClick);
+
+defaultBtnState();
+
+function defaultBtnState() {
+  ref.btnStop.disabled = true;
+  ref.btnStart.disabled = false;
+}
+
+function reverseBtnState() {
+  ref.btnStop.disabled = false;
+  ref.btnStart.disabled = true;
+}
+
+function onBtnStartClick() {
+  timer = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  reverseBtnState();
+  
+  console.log(timer);
+}
+
+function onBtnStopClick() {
+  clearInterval(timer);
+  defaultBtnState();
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const refs = {
+  delayEl: document.querySelector('input[name="delay"]'),
+  stepEl: document.querySelector('input[name="step"]'),
+  amountEl: document.querySelector('input[name="amount"]'),
+  buttonEl: document.querySelector('button[type="submit"]'),
+};
+refs.buttonEl.addEventListener('click', onBtnClick);
+
+function onBtnClick(event) {
+  event.preventDefault();
+
+  const { delayEl, stepEl, amountEl } = refs;
+
+  const amount = Number(amountEl.value);
+  const step = Number(stepEl.value);
+  let delay = Number(delayEl.value);
+
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    createPromise(position, delay)
+      // Fulfill
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      // Reject
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
 }
